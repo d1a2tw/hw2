9b957d10bcd1ab1ba1e4241325addf5b9d1834ef
d1a2tw <d1a2tw@gmail.com>
2020-03-31 22:57:53 +0800

    123

--- try1/main.cpp
+++ try1/main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
 
 
+DigitalIn  Switch(SW3);
+
+DigitalOut redLED(LED1);
+
+DigitalOut greenLED(LED2);
 AnalogOut Aout(DAC0_OUT);
 Serial pc( USBTX, USBRX );
 AnalogIn Ain(A0);
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
-
 char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+char table1[10] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF};
+char clean[1]={0x00};
 
 int sample = 1000;
 float ADCdata[1000];
@@ -14,52 +20,75 @@ float ADCdata[1000];
 int main(){
 
 for(int i=0;i<sample;i++){
-    Aout=Ain;
     ADCdata[i] = Ain;
     wait(1./sample);
                 }
-wait(5);
-/*
-for(int i=100;i<200;i++){
-    pc.printf("i=%d, data= %1.3f\r\n",i, ADCdata[i]);
+
+for(int i=0;i<sample;i++){
+    pc.printf("%1.3f\r\n", ADCdata[i]);
                 }
-wait(90);
-*/
-               
+
+           
 int t1=0;
 int num1=0;
 int num2=0;
 int i=500;
 int freq=0;
 int t2=0;
-while(freq==0){
+wait(5);
+while(freq==0){               
     if(ADCdata[i-1]<ADCdata[i] && ADCdata[i+1]<ADCdata[i]){
         if(num1==0){t1=i;num1=1;}
         else if(num1==1){t2=i;num2=1;}
     }
     
-
-    if(num2==1)freq=1/((t2-t1+1)*0.001);
-    //pc.printf("ADCdata=%.3f , i=%d ,num1=%d ,t1=%d ,t2=%d ,freq=%d\r\n",ADCdata[i],i,num1 ,t1,t2,freq);
+    
+    if(num2==1)freq=1/((t2-t1+1)*0.001);              /////////// calculate freqeuncy
+     
+    pc.printf("freq=%d\r\n",freq);
+    wait(0.1);
     i++;
-   // wait(0.01);
               }
 
-//pc.printf("freq=%d",freq);
 
-        
-/*
-while(1){
-wait(1);
-pc.printf("%d , %d \r\n", t1,t2);
+
+
+//float wait_time=1/(100*freq);  //  T=1/freq ,wait time= period /100 parts
+
+
+//pc.printf("%.8f\n",wait_time);
+
+while(1)
+{
+    if(Switch==0)    // pressed switch=0  , light trun on=0 ,turn off=1
+    { 
+        greenLED=1;
+        redLED=0;
+         
+        int a=freq;
+        int b[3];
+        b[0]=a/100;
+        b[1]=(a-b[0]*100)/10;
+        b[2]=(a-b[0]*100-b[1]*10)/1;
+        for(int i=0;i<3;i++){
+            if(i!=2){display = table[b[i]];}
+            else{display = table1[b[i]];}
+            wait(1);
+                            } 
+    }
+    else
+    {
+        greenLED=0;
+        redLED=1;
+        display = clean[0];
+        /*for(int i=0;i<2;i+=0.02){   //  2pi divides to 100 parts
+            Aout=1+1*sin(i*3.14159);
+            wait(wait_time);     
+                            } */
+    }
+
+
 }
-*/
-
-
-
-
-
-
 
 
 
